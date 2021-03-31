0a308ad308eaeefb02784098d20e2994aef43765
sandip235 <sandipmachhi23@gmail.com>
2021-03-31 23:39:10 +0530

    Update 14DataBaseConnection.php

--- 14DataBaseConnection.php
+++ 14DataBaseConnection.php
@@ -2,12 +2,15 @@
 
 echo "Lec's connect with database";
 
+// To connect with database
 $servername="localhost";
 $username="root";
 $password="";
 
+// Create connection
 $conn= mysqli_connect($servername,$username,$password);
 
+// Die if connection will not successful
 if(!$conn)
 {
     die("<br><br>connection failed<br>".mysqli_connect_error());
