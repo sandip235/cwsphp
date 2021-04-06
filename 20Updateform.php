efd534c5aa947e330d9b518cc1358141c5c32c6d
sandip235 <sandipmachhi23@gmail.com>
2021-04-06 19:55:46 +0530

    Update 20Updateform.php

--- 20Updateform.php
+++ 20Updateform.php
@@ -10,27 +10,45 @@ $database="dbform";
 $conn=mysqli_connect($servername,$username,$password,$database);
 // Connection checking
 
-if(!conn)
+if(!$conn)
 {
-    echo "connection not created";
+    echo "<br>connection not created";
 }
 else
 {
-    echo "Connection successful";
+    echo "<br>Connection successful";
 }
 
-$sql="";
+$sql="select * from `f_tb1`";
 $result=mysqli_query($conn,$sql);
 
-if(!$result)
+$num=mysqli_num_rows($result);
+echo "<br>".$num;
+
+echo "records fetch successfully....<br>";
+if($num>0)
 {
-    echo "Not Updated";
-}
-else
-{
-    echo "Updated Successfully !!";
+    while($row=mysqli_fetch_assoc($result))
+    {
+        echo $row['id'].$row['name'].$row['email'].$row['dsc'];
+    }
 }
 
+echo "<br>Fetch Complete----------------------<br>";
+// Usage of WHERE Clause to Update Data
+$sql1 = "UPDATE `f_tb1` SET `name` = 'FromBihar' WHERE `id` = 4";
+$result1 = mysqli_query($conn, $sql1);
+echo var_dump($result1);
+$aff = mysqli_affected_rows($conn);
+echo "<br>Number of affected rows: $aff <br>";
+
+if($result)
+{
+    echo "We updated the record successfully";
+}
+else{
+    echo "We could not update the record successfully";
+}
 
 
 
